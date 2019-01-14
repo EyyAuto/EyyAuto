ae399f80604ffb2d15eebb03f871cd9f1b0fc43c
rohit-s8 <rohit.sukumaran8@gmail.com>
2019-01-14 23:49:50 +0530

    added basic sign in function [skip ci]

--- app/src/main/java/in/ac/nitc/eyyauto/MainActivity.java
+++ app/src/main/java/in/ac/nitc/eyyauto/MainActivity.java
@@ -1,13 +1,83 @@
 package in.ac.nitc.eyyauto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button signInButton = findViewById(R.id.button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignIn();
+            }
+        });
+    }
+
+    private void SignIn() {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            Toast.makeText(this, "Signing " + mAuth.getCurrentUser().getDisplayName() + " out...", Toast.LENGTH_SHORT).show();
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(MainActivity.this, "Signed out", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        } else {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.GoogleBuilder().build()
+                            )).build(),
+                    RC_SIGN_IN
+            );
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Signed In", Toast.LENGTH_SHORT).show();
+            } else {
+                if (response == null) {
+                    Toast.makeText(this, "Sign in Cancelled", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "Network error", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Toast.makeText(this, "Error trying to Sign in", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
