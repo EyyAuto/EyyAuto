fd39500a7628e6b03351d8dafe8b0a0722d86e3a
Viggnah Selvaraj <viggnah_b150080cs@nitc.ac.in>
2019-02-06 15:37:41 +0530

    Sign IN + Registration

--- app/src/main/java/in/ac/nitc/eyyauto/MainActivity.java
+++ app/src/main/java/in/ac/nitc/eyyauto/MainActivity.java
@@ -2,18 +2,14 @@ package in.ac.nitc.eyyauto;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
@@ -21,41 +17,43 @@ public class MainActivity extends AppCompatActivity {
 
     private static int RC_SIGN_IN = 123;
 
+    private FirebaseUser mUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button signInButton = findViewById(R.id.button);
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SignIn();
-            }
-        });
+        if (isPhoneRegistered()) {
+            Intent intent = new Intent(this, GetName.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            signIn();
+        }
     }
 
-    private void SignIn() {
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        if (mAuth.getCurrentUser() != null) {
-            Toast.makeText(this, "Signing " + mAuth.getCurrentUser().getDisplayName() + " out...", Toast.LENGTH_SHORT).show();
-            AuthUI.getInstance()
-                    .signOut(this)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            Toast.makeText(MainActivity.this, "Signed out", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-        } else {
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(Arrays.asList(
-                                    new AuthUI.IdpConfig.GoogleBuilder().build()
-                            )).build(),
-                    RC_SIGN_IN
-            );
+    private boolean isPhoneRegistered() {
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (mUser != null) {
+            if(mUser.getPhoneNumber() != null) {
+                return true;
+            } else {
+                return false;
+            }
         }
+        return false;
+    }
+
+    private void signIn() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.PhoneBuilder().build()
+                        )).build(),
+                RC_SIGN_IN
+        );
     }
 
     @Override
@@ -65,7 +63,9 @@ public class MainActivity extends AppCompatActivity {
             IdpResponse response = IdpResponse.fromResultIntent(data);
 
             if (resultCode == RESULT_OK) {
-                Toast.makeText(this, "Signed In", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, GetName.class);
+                startActivity(intent);
+                finish();
             } else {
                 if (response == null) {
                     Toast.makeText(this, "Sign in Cancelled", Toast.LENGTH_SHORT).show();
