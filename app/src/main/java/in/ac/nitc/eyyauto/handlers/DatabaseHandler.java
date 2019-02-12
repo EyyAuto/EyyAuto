47cf4702fd2601ee8ea282a9b9aadff45cf6bb43
rohit-s8 <rohit.sukumaran8@gmail.com>
2019-02-12 21:41:47 +0530

    databasehandler as template

--- app/src/main/java/in/ac/nitc/eyyauto/handlers/DatabaseHandler.java
+++ app/src/main/java/in/ac/nitc/eyyauto/handlers/DatabaseHandler.java
@@ -5,27 +5,56 @@ import android.support.annotation.NonNull;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
-public abstract class DatabaseHandler {
+public abstract class DatabaseHandler<T> {
     private static final FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-    protected void getDataOnce(String path, final Class<?> type) {
+    private void processResponse(@NonNull DataSnapshot dataSnapshot, @NonNull Event<T> event) {
+        GenericTypeIndicator<T> t = new GenericTypeIndicator<T>() {
+        };
+        T data = dataSnapshot.getValue(t);
+        event.onReceive(data);
+    }
+
+    protected void getDataOnce(@NonNull String path, @NonNull final Event<T> event) {
         database.getReference(path).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Object data = dataSnapshot.getValue(type);
-                onReceive(data);
+                processResponse(dataSnapshot, event);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                onFailed(databaseError);
+                event.onFailed(databaseError);
             }
         });
     }
 
-    abstract protected void onReceive(Object data);
+    protected ValueEventListener addListener(@NonNull String path, @NonNull final Event<T> listener) {
+        return database.getReference(path).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                processResponse(dataSnapshot, listener);
+            }
 
-    abstract protected void onFailed(DatabaseError error);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                listener.onFailed(databaseError);
+            }
+        });
+    }
+
+    protected void removeListener(@NonNull String path, @NonNull ValueEventListener listener) {
+        database.getReference(path).removeEventListener(listener);
+    }
+
+    protected void putData(@NonNull String path, @NonNull T data) {
+        database.getReference(path).setValue(data);
+    }
+
+    abstract public void readOnce(@NonNull String uid, @NonNull Event<T> event);
+
+    abstract public void putValue(@NonNull String uid, @NonNull T data);
 }
--- app/src/main/java/in/ac/nitc/eyyauto/handlers/UserHandler.java
+++ app/src/main/java/in/ac/nitc/eyyauto/handlers/UserHandler.java
@@ -2,30 +2,21 @@ package in.ac.nitc.eyyauto.handlers;
 
 import android.support.annotation.NonNull;
 
-import com.google.firebase.database.DatabaseError;
-
 import in.ac.nitc.eyyauto.models.User;
 
 import static in.ac.nitc.eyyauto.Constants.USER_INFO_ROOT_PATH;
 
-public final class UserHandler extends DatabaseHandler {
-    private static final Class<?> type = User.class;
-    private Event<User> event;
+public final class UserHandler extends DatabaseHandler<User> {
 
-    public void get(@NonNull String uid, @NonNull Event<User> event) {
+    @Override
+    public void readOnce(@NonNull String uid, @NonNull Event<User> event) {
         String path = USER_INFO_ROOT_PATH + uid;
-        this.event = event;
-        getDataOnce(path, type);
+        getDataOnce(path, event);
     }
 
     @Override
-    protected void onReceive(Object data) {
-        User user = (User) data;
-        event.onReceive(user);
-    }
-
-    @Override
-    protected void onFailed(DatabaseError error) {
-        event.onFailed(error);
+    public void putValue(@NonNull String uid, @NonNull User data) {
+        String path = USER_INFO_ROOT_PATH + uid;
+        putData(path, data);
     }
 }
